7b5205c033abf7c613a8cb54d0ed2adaa94af7b1
Uberlyuber <ehanlon19@gmail.com>
2016-02-21 17:15:45 -0800

    Delete deathHandler.lua
    This beongs as a LocalScript, dumb me

--- ServerScriptService/deathHandler.lua
+++
@@ -1,8 +0,0 @@
-while true do
-	for k,v in pairs(game.Players:GetChildren()) do
-		if game.Players.LocalPlayer.Character.Humanoid.Health == 0 and not game.ReplicatedStorage.timeLeft.Value == 0 then
-			v.TeamColor = BrickColor.new("Really black")
-		end
-	end
-	wait(1)
-end
